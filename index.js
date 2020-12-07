45e65006b0d8d1b3552ccee5989df157d0a1e140
root <root@localhost.localdomain>
2020-12-07 12:39:59 +0100

    update

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.post('/startrandom', (req, res) => {
         }, (i*200))
     }
 })
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
